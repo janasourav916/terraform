b437cbed43668f3826dd8a00ec8a84be9efae5ed
Sourav Jana <sourav.jana@nextbee.net>
2024-12-11 19:12:44 +0530

    update

--- aws/backend.tf
+++ aws/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = var.bucket
-    key    = var.key
+    bucket = "sjana-terraform-states"
+    key    = "create-ec2/terraform.tfstate"
     region = "ap-south-1"
   }
 }
