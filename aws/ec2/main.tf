e27980b677f3118d117f353233353a454a02d8ee
Sourav Jana <sourav.jana@nextbee.net>
2024-12-11 16:41:03 +0530

    add remote state

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -6,3 +6,57 @@ resource "aws_instance" "app_server" {
     Name = "Terraform_Demo"
   }
 }
+
+# ------------------------------------------------------------------------------
+# CREATE THE S3 BUCKET
+# ------------------------------------------------------------------------------
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  # With account id, this S3 bucket names can be *globally* unique.
+  bucket = "${local.account_id}-terraform-states"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# ------------------------------------------------------------------------------
+# CREATE THE DYNAMODB TABLE
+# ------------------------------------------------------------------------------
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
