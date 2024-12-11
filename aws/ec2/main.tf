1f4f4b64d02eba78f167bb15cbae4c5675fb934e
Sourav Jana <sourav.jana@nextbee.net>
2024-12-11 18:24:50 +0530

    update

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -18,21 +18,21 @@ locals {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  # With account id, this S3 bucket names can be *globally* unique.
+  # With account id, this S3 bucket name can be *globally* unique.
   bucket = "${local.account_id}-terraform-states"
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
+  # Enable versioning so we can see the full revision history of our state files.
   versioning {
     enabled = true
   }
+}
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
